db2beefba645440b2e64fb8340a4e4378c196f29
aspirantzhang <admin@aspirantzhang.com>
2020-08-28 23:33:21 +0800

    WIP: Link list data & add data

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -1,60 +1,209 @@
-import React, { FC } from 'react';
-import { Form, Input, Button, message, Icon } from 'antd';
-import { MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { useDynamicList } from 'ahooks';
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Card } from 'antd';
+import {
+  createFormActions,
+  SchemaForm,
+  SchemaMarkupField as Field,
+  FormButtonGroup,
+  Submit,
+  Reset,
+  FormEffectHooks,
+  FormPath,
+} from '@formily/antd';
+import { Input, ArrayTable, Select, Checkbox, FormCard } from '@formily/antd-components';
+import 'antd/dist/antd.css';
+
+const actions = createFormActions();
+const { onFieldValueChange$ } = FormEffectHooks;
 
 const Index = () => {
-  const [form] = Form.useForm();
-  const { list, remove, getKey, push } = useDynamicList(['David', 'Jack']);
-
-  const onFinish = (values) => {
+  const submitHandler = (values) => {
     console.log(values);
-    message.info(JSON.stringify(values));
   };
 
-  const buildRow = (index: number, item: any) => {
+  const actionFields = () => {
     return (
-      <>
-        <Form.Item key={getKey(index)} required={false}>
-          <Form.Item {...item} noStyle>
-            <Input style={{ width: 300 }} />
-          </Form.Item>
-          <MinusCircleOutlined
-            style={{ marginLeft: 8 }}
-            onClick={() => {
-              remove(index);
-            }}
-          />
-          <PlusCircleOutlined
-            style={{ marginLeft: 8 }}
-            onClick={() => {
-              push('');
-            }}
-          />
-        </Form.Item>
-      </>
+      <Field type="object">
+        <Field name="name" x-component="Input" title="Name*" />
+        <Field
+          name="type"
+          x-component="Select"
+          title="Type*"
+          default="primary"
+          enum={[
+            { label: 'Primary', value: 'primary' },
+            { label: 'Default', value: 'default' },
+            { label: 'Dashed', value: 'dashed' },
+          ]}
+        />
+        <Field
+          name="action"
+          x-component="Select"
+          title="Action*"
+          default="modal"
+          enum={[
+            { label: 'Modal', value: 'modal' },
+            { label: 'Page', value: 'page' },
+            { label: 'Delete', value: 'delete' },
+            { label: 'Reset', value: 'reset' },
+            { label: 'Submit', value: 'submit' },
+          ]}
+        />
+        <Field name="uri" x-component="input" title="Uri" />
+        <Field
+          name="method"
+          x-component="Select"
+          title="Method*"
+          default="get"
+          enum={[
+            { label: 'Get', value: 'get' },
+            { label: 'Post', value: 'post' },
+            { label: 'Delete', value: 'delete' },
+          ]}
+        />
+      </Field>
     );
+  };
+
+  const formEffects = ({ setFieldValue }) => {
+    onFieldValueChange$('fields.*.listData').subscribe(({ name, value }) => {
+      if (value) {
+        setFieldValue(
+          FormPath.transform(name, /\d/, ($1) => {
+            return `fields.${$1}.addData`;
+          }),
+          true,
+        );
+      }
+    });
   };
 
   return (
     <>
-      <Form form={form} name="control-hooks" onFinish={onFinish}>
-        <Form.List name="names">
-          {list.map((ele, index) => {
-            console.log(ele, index);
-            // return buildRow(index, ele);
-          })}
-        </Form.List>
-      </Form>
-      <Button
-        style={{ marginTop: 8 }}
-        type="primary"
-        onClick={() => {
-          form.submit();
-        }}
-      >
-        Submit
-      </Button>
+      <PageHeaderWrapper>
+        <Card>
+          <SchemaForm
+            actions={actions}
+            components={{ ArrayTable, Input, Select, Checkbox }}
+            onSubmit={submitHandler}
+            effects={formEffects}
+          >
+            <FormCard title="Fields">
+              <Field
+                name="fields"
+                type="array"
+                default={[]}
+                x-component="ArrayTable"
+                x-component-props={{
+                  operationsWidth: 150,
+                  operations: {
+                    title: '',
+                  },
+                  draggable: true,
+                }}
+              >
+                <Field type="object">
+                  <Field name="name" x-component="Input" title="Name*" />
+                  <Field name="title" x-component="Input" title="Title*" />
+                  <Field
+                    name="type"
+                    x-component="Select"
+                    title="Type*"
+                    default="text"
+                    enum={[
+                      { label: 'Text', value: 'text' },
+                      { label: 'Tree', value: 'tree' },
+                      { label: 'Datetime', value: 'datetime' },
+                    ]}
+                  />
+                  <Field name="listData" x-component="Checkbox" title="List: Data" />
+                  <Field
+                    name="listHideInColumn"
+                    x-component="Checkbox"
+                    title="List: HideInColumn"
+                  />
+                  <Field name="listSorter" x-component="Checkbox" title="List: Sorter" />
+                  <Field name="addData" x-component="Checkbox" title="Add: Data" />
+                  <Field name="editDisabled" x-component="Checkbox" title="Edit: Disabled" />
+                </Field>
+              </Field>
+            </FormCard>
+            <FormCard title="List Action">
+              <Field
+                name="listAction"
+                type="array"
+                default={[]}
+                x-component="ArrayTable"
+                x-component-props={{
+                  operationsWidth: 150,
+                  operations: {
+                    title: 'Action',
+                  },
+                  draggable: true,
+                }}
+              >
+                {actionFields()}
+              </Field>
+            </FormCard>
+            <FormCard title="Add Action">
+              <Field
+                name="addAction"
+                type="array"
+                default={[]}
+                x-component="ArrayTable"
+                x-component-props={{
+                  operationsWidth: 150,
+                  operations: {
+                    title: '',
+                  },
+                  draggable: true,
+                }}
+              >
+                {actionFields()}
+              </Field>
+            </FormCard>
+            <FormCard title="Table Toolbar">
+              <Field
+                name="tableToolBar"
+                type="array"
+                default={[]}
+                x-component="ArrayTable"
+                x-component-props={{
+                  operationsWidth: 150,
+                  operations: {
+                    title: '',
+                  },
+                  draggable: true,
+                }}
+              >
+                {actionFields()}
+              </Field>
+            </FormCard>
+            <FormCard title="Batch Toolbar">
+              <Field
+                name="batchToolBar"
+                type="array"
+                default={[]}
+                x-component="ArrayTable"
+                x-component-props={{
+                  operationsWidth: 150,
+                  operations: {
+                    title: '',
+                  },
+                  draggable: true,
+                }}
+              >
+                {actionFields()}
+              </Field>
+            </FormCard>
+            <FormButtonGroup offset={5}>
+              <Submit>查询</Submit>
+              <Reset>重置</Reset>
+            </FormButtonGroup>
+          </SchemaForm>
+        </Card>
+      </PageHeaderWrapper>
     </>
   );
 };
