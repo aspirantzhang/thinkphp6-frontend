6c1210dac6a8efe2aa2ec3802c9576b7f743fadb
aspirantzhang <admin@aspirantzhang.com>
2020-10-29 15:43:12 +0800

    fix: fix errors reported by unit test

--- src/pages/BasicList/helper.ts
+++ src/pages/BasicList/helper.ts
@@ -6,15 +6,19 @@ import { join } from 'lodash';
  */
 export const buildSorter = (sorter: any) => {
   let sorterQuery: string = '';
-  if (sorter.order !== undefined || sorter.order !== null) {
-    sorterQuery = `&sort=${sorter.field}&order=${sorter.order === 'ascend' ? 'asc' : 'desc'}`;
+  if (sorter) {
+    sorterQuery = `${sorter.field ? '&sort=' + sorter.field : ''}${
+      sorter.order ? (sorter.order === 'ascend' ? '&order=asc' : '&order=desc') : ''
+    }`;
+    // sorterQuery = `&sort=${sorter.field}&order=${sorter.order === 'ascend' ? 'asc' : 'desc'}`;
   }
   return `${sorterQuery}`;
 };
 
 export const buildUriMatch = (match: any) => {
-  const params = match.params;
-  const fullUri = join(Object.values(params), '/');
+  const params =
+    typeof match === 'object' && match !== null ? (match.params ? match.params : {}) : {};
+  const fullUri = typeof params === 'object' ? join(Object.values(params), '/') : '';
   const baseUri = { app: '', controller: '', action: '' };
   return { fullUri, ...baseUri, ...params };
 };
