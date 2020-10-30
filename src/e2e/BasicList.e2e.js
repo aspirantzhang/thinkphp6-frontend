c42b85419a5e2950948742a2c3d54a08b255fa47
aspirantzhang <admin@aspirantzhang.com>
2020-10-31 00:33:26 +0800

    test: add more test for BasicList

--- src/e2e/BasicList.e2e.js
+++ src/e2e/BasicList.e2e.js
@@ -7,8 +7,8 @@ afterEach(async () => {});
 describe('BasicList', () => {
   it('basic list', async () => {
     const browser = await puppeteer.launch({
-      // headless: false,
-      // slowMo: 20,
+      headless: false,
+      slowMo: 20,
       timeout: 5000,
     });
 
@@ -20,6 +20,7 @@ describe('BasicList', () => {
     await page.click('button[type="submit"]');
     await page.waitForNavigation();
     await page.goto(`${BASE_URL}/basic-list/backend/admins`);
+    // Add
     await page.waitForSelector('.ant-pro-page-container');
     await page.waitForSelector('#add');
     await page.click('#add');
@@ -28,44 +29,81 @@ describe('BasicList', () => {
     await page.type('.ant-modal-content #username', 'unitTestUser');
     await page.type('.ant-modal-content #password', 'unitTestPass');
     await page.click('.ant-modal-body .ant-btn.ant-btn-primary');
-    await page.waitFor(3000);
+    await page.waitForTimeout(3000);
     expect(
       await page.$eval('tbody tr:nth-child(1) td:nth-child(3)', (node) => node.innerText),
     ).toBe('unitTestUser');
 
+    // // Modal Edit
     await page.waitForSelector('.ant-pro-page-container');
+    await page.waitForTimeout(2000);
+    await page.click(
+      'tbody tr:nth-child(1) td:nth-child(7) div:nth-child(1) div:nth-child(1) button:nth-child(1)',
+    );
+    await page.waitForTimeout(2000);
+    expect(await page.$eval('.ant-modal-body #username', (node) => node.value)).toBe(
+      'unitTestUser',
+    );
+    await page.type('.ant-modal-body #display_name', 'UserDisplayName');
+    await page.click('.ant-modal-body .ant-btn.ant-btn-primary');
+    await page.waitForTimeout(2000);
+    expect(
+      await page.$eval('tbody tr:nth-child(1) td:nth-child(4)', (node) => node.innerText),
+    ).toBe('UserDisplayName');
 
-    await page.waitFor(2000);
+    // Full page Edit
+    await page.waitForSelector('.ant-pro-page-container');
+    await page.waitForTimeout(2000);
+    await page.click(
+      'tbody tr:nth-child(1) td:nth-child(7) div:nth-child(1) div:nth-child(2) button:nth-child(1)',
+    );
+    await page.waitForSelector('.ant-pro-page-container');
+    await page.waitForTimeout(1000);
+    expect(await page.$eval('#username', (node) => node.value)).toBe('unitTestUser');
+    await page.type('#display_name', '2');
+    await page.waitForTimeout(1000);
+    await page.click('.ant-pro-footer-bar-left .ant-btn-primary');
+    await page.waitForTimeout(2000);
+    await page.waitForSelector('.ant-pro-page-container');
+    await page.waitForTimeout(1000);
+    expect(
+      await page.$eval('tbody tr:nth-child(1) td:nth-child(4)', (node) => node.innerText),
+    ).toBe('UserDisplayName2');
+
+    // // Delete
+    await page.waitForSelector('.ant-pro-page-container');
+    await page.waitForTimeout(2000);
     await page.click(
       'tbody tr:nth-child(1) td:nth-child(7) div:nth-child(1) div:nth-child(3) button:nth-child(1)',
     );
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
     await page.click('.ant-modal-confirm-btns .ant-btn-dangerous');
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
     expect(
       await page.$eval('tbody tr:nth-child(1) td:nth-child(3)', (node) => node.innerText),
     ).not.toBe('unitTestUser');
 
+    // // Delete Permanently
     await page.waitForSelector('#searchExpandButton');
     await page.click('#searchExpandButton');
     await page.click('#trash');
     await page.waitForSelector("div[title='Only Trashed']");
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
     await page.click("div[title='Only Trashed']");
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
     await page.click('#searchSubmit');
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
 
     await page.waitForSelector('.ant-pro-page-container');
 
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
 
     await page.click(
       'tbody tr:nth-child(1) td:nth-child(1) label:nth-child(1) span:nth-child(1) input:nth-child(1)',
     );
     await page.waitForSelector("button[class='ant-btn ant-btn-danger']");
     await page.click("button[class='ant-btn ant-btn-danger']");
-    await page.waitFor(2000);
+    await page.waitForTimeout(2000);
     await page.waitForSelector("button[class='ant-btn ant-btn-dangerous']");
     await page.click("button[class='ant-btn ant-btn-dangerous']");
 
