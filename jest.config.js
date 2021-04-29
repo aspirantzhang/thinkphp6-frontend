d58111e1947fd56b3bc7703fa2a4d20ce6cfba21
aspirantzhang <admin@aspirantzhang.com>
2021-04-29 19:13:13 +0800

    test: config e2e test

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,19 @@ module.exports = {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
     localStorage: null,
   },
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '.umi',
+    'services',
+    '.test.tsx',
+    '.test.ts',
+    '.d.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@@/(.*)$': '<rootDir>/src/.umi/$1',
+  },
 };
--- src/e2e/BasicList.e2e.js
+++ src/e2e/BasicList.e2e.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import * as pti from 'puppeteer-to-istanbul';
 
 test('BasicList', async () => {
   const browser = await puppeteer.launch({
@@ -6,18 +7,40 @@ test('BasicList', async () => {
     slowMo: 20,
   });
   const page = await browser.newPage();
+
+  // Enable both JavaScript and CSS coverage
+  await Promise.all([page.coverage.startJSCoverage(), page.coverage.startCSSCoverage()]);
+
+  // login
   await page.goto('http://localhost:8000/user/login');
   await page.waitForSelector('#username');
-  await page.type('#username', 'admin0');
-  await page.type('#password', 'admin0');
+  await page.type('#username', 'admin');
+  await page.type('#password', 'admin');
   await page.click("button[class='ant-btn ant-btn-primary ant-btn-lg']");
   await page.waitForNavigation();
   await page.goto('http://localhost:8000/basic-list/api/admins');
+
+  // add
   await page.waitForSelector("button[class='ant-btn ant-btn-primary btn-add'] span");
   await page.click("button[class='ant-btn ant-btn-primary btn-add'] span");
-  await page.waitForSelector('#username');
-  await page.type('#username', 'testUsername');
+  await page.waitForSelector('#admin_name');
+  await page.type('#admin_name', 'e2eUser');
+  await page.type('#password', 'e2ePass');
+  await page.click("button[class='ant-btn ant-btn-primary btn-submit']");
+  expect(await page.$eval('tbody tr:nth-child(1) td:nth-child(3)', (node) => node.innerText)).toBe(
+    'e2eUser',
+  );
+
+  // Disable both JavaScript and CSS coverage
+  const [jsCoverage, cssCoverage] = await Promise.all([
+    page.coverage.stopJSCoverage(),
+    page.coverage.stopCSSCoverage(),
+  ]);
+  pti.write([...jsCoverage, ...cssCoverage], {
+    includeHostname: true,
+    storagePath: './.nyc_output',
+  });
 
   await page.close();
-  browser.close();
+  await browser.close();
 });
